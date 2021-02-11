10916d500ca62f0ba7011a1a1df16255890ab6c5
SDGreen <sad_green@hotmail.co.uk>
2021-02-11 16:06:53 +0000

    quiz model added

--- quizzes/models.py
+++ quizzes/models.py
@@ -11,3 +11,14 @@ class Category(models.Model):
 
     def get_friendly_name(self):
         return self.friendly_name
+
+
+class Quiz(models.Model):
+    name = models.CharField(max_length=200)
+    display_name = models.CharField(max_length=400)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    description = models.TextField
+    rating = models.DecimalField(max_digits=2, decimal_places=1, null=True, blank=True)
+    img_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(upload_to="images/", null=True, blank=True)
+    quiz_file = models.FileField(upload_to="quizzes/")
